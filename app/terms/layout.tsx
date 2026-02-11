9ccb14e1add6f4936481c1dca7ab909765f248ad
r <rin5uron@gmail.com>
2026-02-11 10:51:17 +0900

    広告幅調整

--- app/terms/layout.tsx
+++ app/terms/layout.tsx
@@ -41,7 +41,7 @@ export default function TermsLayout({
     <div className="terms-layout">
       <div className="terms-main">
         {children}
-        <div className="container related-books-container">
+        <div className="related-books-container">
           <RelatedBooks books={DEFAULT_BOOKS} />
         </div>
         <AdInArticle adSlot={SLOT_FOOTER} position="footer" />
